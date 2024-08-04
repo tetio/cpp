fd420805d75f666690ab99d1422dc2150aaca6f4
tetio <tetio@live.com>
2024-08-04 19:43:02 +0200

    init

---
+++ lasagna/lasagna.cpp
@@ -0,0 +1,19 @@
+int ovenTime()
+{
+    return 40;
+}
+
+int remainingOvenTime(int actualMinutesInOven)
+{
+    return ovenTime() - actualMinutesInOven;
+}
+
+int preparationTime(int numberOfLayers)
+{
+    return 2 * numberOfLayers;
+}
+
+int elapsedTime(int numberOfLayers, int actualMinutesInOven)
+{
+    return preparationTime(numberOfLayers) + actualMinutesInOven;
+}
